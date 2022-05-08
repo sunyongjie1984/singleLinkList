0c5f40053ac770247c8c3539f924fa119b4151bd
yongjies <yongjies@cadence.com>
2022-05-08 23:47:51 +0800

    modify

---
+++ singleLinkList/MyException.h
@@ -0,0 +1,12 @@
+#include <exception>
+#include <string.h>
+
+class MyException: public std::exception {
+public:
+    MyException() throw(){ memset(m_str, 0, 256 * sizeof(char)); }
+    MyException(char const * p) throw() { memset(m_str, 0, 256 * sizeof(char)); strcpy(m_str, p); }
+    char const * what() const throw() { return "hi what what()"; /*std::cout << m_str << std::endl;*/ }
+    ~MyException() throw() {}
+private:
+    char m_str[256];
+};
